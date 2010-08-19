59207694c5b427f5fbaebb4237091d257569b2be
nybras <nybras@codeine.it>
2010-08-19 18:55:43 -0300

    adding basic stubs for Larva

--- src/dragonfly/core/Larva.hx
+++ src/dragonfly/core/Larva.hx
@@ -1,9 +1,22 @@
 package dragonfly.core;
 
+import dragonfly.core.Egg;
+
+import flash.utils.TypedDictionary;
+
 class Larva {
+	
+	public var _initialized : Bool;
+	public var target : Dynamic;
+	public var eggs : TypedDictionary<Egg,Float>;
+	
 	public function new()
 	{
-		trace( "Larva!" );
-		new Egg();
+		eggs = new TypedDictionary();
+	}
+	
+	public function kill_flying_properties( egg : Egg, props : Array<String> ) : Void
+	{
+		
 	}
 }
